7855da39ca77db7601492dba1bfb06b13f42b59a
anthobldr <anthony.lutard33@gmail.com>
2024-01-21 13:19:36 +0100

    Site vitrine GRAD.

--- assets/components/navbar.php
+++ assets/components/navbar.php
@@ -26,4 +26,5 @@
         </ul>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script><div class="collapse navbar-collapse justify-content-end">
+      </div>
